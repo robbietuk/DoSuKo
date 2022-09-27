77561667ad8ac69ce1ff9cc87acf43f9f4603fef
Robert Twyman Skelly <robbietuk@gmail.com>
2022-09-27 14:32:11 -0700

    Fixes to main

--- main.cpp
+++ main.cpp
@@ -7,55 +7,50 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    std::cout << "Usage: " << argv[0] << " <file_name>\n";
+    std::cout << "Usage:\n\t" << argv[0] << " <file_name>\n";
     return 1;
   }
 
   int num_solved_boards = 0;
-
   LoadBoards board_configs(argv[1]);
-
   const int num_problems_loaded = board_configs.get_num_problems_loaded();
+  
   for (int i = 0; i < num_problems_loaded; i++) {
-    ProblemSolutionSet problem_solution_set = board_configs.get_next_problems_set();
+    ProblemSolutionSet pss = board_configs.get_next_problems_set();
 
-    SudokuBoard<PotentialCell> board(problem_solution_set.problem);
+    SudokuBoard<PotentialCell> board(pss.problem);
 
-    if (!board_configs.is_problem_solution_combo_valid(problem_solution_set.problem,
-                                                       problem_solution_set.solution)) {
-      std::cerr << "WARNING: The solution does not represent the problem!\n"
-                   "Problem  "
-                << i << ": " << problem_solution_set.problem << "\n"
-                                                                "Solution "
-                << i << ": " << problem_solution_set.solution << "\n";
+    if (!board_configs.is_problem_solution_combo_valid(pss.problem, pss.solution)) {
+      std::cerr << i << " WARNING: The solution does not represent the initial loaded problem!\n"
+                        "Problem:  " << pss.problem << "\n"
+                        "Solution: " << pss.solution << "\n";
       continue;
     }
 
-    // Solve the board
+    // Solve the board using DumbOne
     DumbOne dumb_one(&board);
     dumb_one.solve_board();
 
     if (dumb_one.is_board_marked_as_solved()) {
-      if (board_configs.is_problem_solution_combo_valid(dumb_one.get_board_ptr()->encode(),
-                                                        problem_solution_set.solution)) {
+      if (board_configs.is_problem_solution_combo_valid(dumb_one.get_board_ptr()->encode(), pss.solution)) {
         num_solved_boards++;
         std::cout << i << ": Solved!\n";
-      } else {
-        std::cerr << "WARNING: The board is marked as solved but the solution does not represent the problem!\n"
-                     "Problem  "
-                  << i << ": " << problem_solution_set.problem << "\n"
-                                                                  "Solution "
-                  << i << ": " << problem_solution_set.solution << "\n";
+      } 
+      else 
+      { 
+        std::cerr << i << " WARNING: The board is marked as solved but the solution does not represent the problem!\n"
+                            "Problem:  " << pss.problem << "\n"
+                            "Solution: " << pss.solution << "\n";
         dumb_one.get_board_ptr()->print_board_2D_visualisation();
       }
-    } else {
-      std::cerr << "WARNING: Board is not solved!\n"
-                   "Problem  "
-                << i << ": " << problem_solution_set.problem << "\n"
-                                                                "Solution "
-                << i << ": " << problem_solution_set.solution << "\n";
+    } 
+    else
+    {
+      std::cerr << i << " WARNING: Board is not solved!\n"
+                        "Problem:  " << pss.problem << "\n"
+                        "Solution: " << pss.solution << "\n";
     }
   }
-  std::cout << "Solved " << num_solved_boards << " / " << num_problems_loaded << " boards.\n";
+  std::cout << "\nSolved " << num_solved_boards << " / " << num_problems_loaded << " boards.\n";
   return 0;
 }
